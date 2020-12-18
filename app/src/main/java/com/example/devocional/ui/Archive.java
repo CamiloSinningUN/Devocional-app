073f7af24c1bd39d82e0d84dcc9380f1716fbc7d
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2020-12-18 17:12:59 -0500

    Deleted some unused parameters

--- app/src/main/java/com/example/devocional/ui/Archive.java
+++ app/src/main/java/com/example/devocional/ui/Archive.java
@@ -6,20 +6,13 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.example.devocional.MainActivity;
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import static android.content.Context.MODE_PRIVATE;
 
